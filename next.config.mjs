93d68fb7af7881840b1d879375052f0b976e3c11
Akkil MG <akkilcharanmg@gmail.com>
2025-08-19 21:45:25 +0530

    Damn try again

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+// import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -29,8 +29,8 @@ const nextConfig = {
   },
 };
 
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   await setupDevPlatform();
+// }
 
 export default nextConfig;
